122e88893ef9e53af468e51d77c977daa7b93787
Thiago <73863469+ThiagoFullStack@users.noreply.github.com>
2024-11-07 20:51:51 -0300

    Some final adjustments

--- src/components/Main/SectionOne/index.tsx
+++ src/components/Main/SectionOne/index.tsx
@@ -4,9 +4,9 @@ export default function SectionOne() {
   return (
     <section className="content">
       <div className="text-content">
-        <h4>No more waste</h4>
-        <h1>Pick the Sun</h1>
-        <h3>
+        <h4 className="textH4">No more waste</h4>
+        <h1 className="textH1">Pick the Sun</h1>
+        <h3 className="textH3">
           Et pulvinar nec interdum integer id urna molestie porta nullam. A, donec ornare sed turpis pulvinar
           <br />
           purus maecenas quam a. Erat prottitor pharetra sed in mauris elementum sollicitudin.
